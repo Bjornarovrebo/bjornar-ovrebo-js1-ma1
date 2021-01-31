68424779c1a45194292c8bf4960ac4950040b832
Bjornarovrebo <71792583+Bjornarovrebo@users.noreply.github.com>
2021-01-31 20:35:26 +0100

    Javascript MA1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21,
+    }
+];
+
+//question 1 
+
+const cat = {
+    complain: "Meow!",
+};
+
+console.log(cat.complain);
+
+//question 2
+
+document.querySelector("h3");
+
