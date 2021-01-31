7d7c541e91079d7f1c5d83e005d3558c34ff3995
Bjornarovrebo <71792583+Bjornarovrebo@users.noreply.github.com>
2021-01-31 23:27:12 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const cats = [
     {
         name: "Blob",
-        age: 10
+        age: 10,
     },
     {
         name: "Harold",
@@ -22,5 +22,68 @@ console.log(cat.complain);
 
 //question 2
 
-document.querySelector("h3");
+const heading = document.querySelector("h3");
+
+heading.innerHTML = ("Updated Heading");
+
+//question 3
+
+heading.style.fontSize = "2em";
+
+//question 4
+
+heading.classList.add ("sub-heading"); 
+
+//question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = ("red");
+}
+
+//question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = `<p style = "background: Yellow"> New Paragraph </p>`;
+
+
+//question 7
+
+function catNames(list) {
+    for(let i = 0; i < cats.length; i++) {
+        console.log(list[i].name);
+    }
+}
+catNames(cats);
+
+//question 8
+
+function createCats(Cats) {
+
+    let catsItems = "";
+
+    for(let i = 0; i < cats.length; i++) {
+
+        let catsAge = "Nobody knows";
+
+        if(cats.age)
+
+        catsItems = catsItems + cats[i];
+    }
+
+
+}
+
+createCats(cats);
+
+
+
+
+
+
+
+
+
 
